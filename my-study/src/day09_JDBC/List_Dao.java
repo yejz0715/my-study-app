56e8bdb6e403411100fcdcbd00b5e0511f352ad1
yejz0715 <yejz0715@gmail.com>
2021-10-29 13:59:22 +0900

    Docs:test_jdbc

--- my-study/src/day09_JDBC/List_Dao.java
+++ my-study/src/day09_JDBC/List_Dao.java
@@ -1,5 +1,72 @@
 package day09_JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class List_Dao {
+	String driver="oracle.jdbc.driver.OracleDriver";
+	String url="jdbc:oracle:thin:@localhost:1521:xe";
+	    Connection con = null;  
+	    PreparedStatement pstmt = null; 
+		ResultSet rs = null; 
+		
+		public  Connection getConnection() {
+			Connection con=null;
+			try {
+			  Class.forName(driver);
+			  con = DriverManager.getConnection(url, "scott", "tiger");
+			    } catch (ClassNotFoundException e) {e.printStackTrace();
+				} catch (SQLException e) {e.printStackTrace(); }	
+			return con;
+		}	
+		public void close() {
+			try {
+				if(con != null) con.close();
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
+			}catch (SQLException e) {e.printStackTrace();
+		 }	
+	   }
+		public ArrayList<List_Dto> select() {
+			ArrayList<List_Dto>list=new ArrayList<List_Dto>();
+			con=getConnection();
+			String sql="select * from List";
+			
+			try {
+				pstmt=con.prepareStatement(sql);
+				rs=pstmt.executeQuery();
+				while(rs.next()) {
+					List_Dto ldto=new List_Dto();
+					ldto.setName(rs.getString("name"));
+					ldto.setAge(rs.getInt("age"));
+					ldto.setPhone(rs.getString("phone"));
+				}
+			}catch(SQLException e) {e.printStackTrace();   
+			} 
+			close();
+			return list;
+		}
+		public int insert(List_Dto ldto) {
+			int result=0;
+			String sql="insert into List value(?,?,?)";
+			con=getConnection();
+			try{
+				pstmt=con.prepareStatement(sql);
+				pstmt.setString(1, ldto.getName());
+				pstmt.setInt(2, ldto.getAge());
+				pstmt.setString(3, ldto.getPhone());
+				result=pstmt.executeUpdate();
+			}catch (SQLException e) {e.printStackTrace();  }	
+			close();			
+		return result;
+		}
+		
+
+
+
 
 }
--- my-study/src/day09_JDBC/List_Driver.java
+++ my-study/src/day09_JDBC/List_Driver.java
@@ -1,6 +1,8 @@
 package day09_JDBC;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class List_Driver {
 
@@ -27,13 +29,33 @@ public class List_Driver {
 	}
 
 	private static void select() {
-		// TODO Auto-generated method stub
-		
+		List_Dao ldao=new List_Dao();		
+		ArrayList<List_Dto>list=ldao.select();
+		System.out.println("\t 이름\t\t나이\t 전화번호");
+		System.out.println("----------------------------------------------------");	
+	for(List_Dto dto:list) {
+		System.out.printf("%s\t%d\t%s",
+				dto.getName(), dto.getAge(), dto.getPhone() );		
+	  }	
 	}
 
 	private static void insert() {
-		// TODO Auto-generated method stub
+		List_Dao ldao=new List_Dao();
+		Scanner sc=new Scanner(System.in);
+		List_Dto ldto=new List_Dto();
 		
+		System.out.print("이름을 입력하세요");
+		ldto.setName( sc.nextLine());
+		
+		System.out.print("나이를 입력하세요");
+		ldto.setAge( sc.nextInt());
+		
+		System.out.print("전화번호를 입력하세요");
+		ldto.setPhone( sc.nextLine());
+		
+		int result=ldao.insert(ldto);
+		if(result==1 )System.out.println("레코드 추가 성공");
+		   else System.out.println("레코드 추가 실패");	
 	}
 
 	private static void update() {
