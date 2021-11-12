6ca903e4c52eac13a7a8f60f19b372f371596006
yejz0715 <yejz0715@gmail.com>
2021-11-13 02:35:12 +0900

    add activation

--- yeji_project/src/main/webapp/resources/js/join.js
+++ yeji_project/src/main/webapp/resources/js/join.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	// duplicate 라는 이름은 어울리지 않음!
 
 	$('.join_input_password').focusout(function() {
-		
+
 		var userPwd = $("#pwd").val();
 		var userPwd_chk = $("#pwd_check").val();
 
@@ -16,34 +16,76 @@ $(document).ready(function() {
 		else $('#guide_pwd').css("color", "#FF0000").text("비밀번호가 일치하지 않습니다. 다시 입력해주세요!")
 
 	});
+	
 });
-
-
-
 
 
 // 아이디 중복 확인 
 function duplicateId() { // 중복확인버튼 클릭하면 duplicateId함수실행      //활성화버튼1
-	console.log(`duplicateId()...`) //콘솔로그 많이 사용하기
+	console.log(`duplicateId()...`); //콘솔로그 많이 사용하기
 	var userId = $("#id").val(); //1 입력한 아이디값 받아오기 #id의 값을 userId에 전달/ $("#id").val(); : 값얻어오기
 
-	if (userId == 'admin') $('#guide_id').css("color", "#FF0000").text("이미 등록된 아이디 입니다. 다시 입력해주세요!")
-	//입력값이 admin이라면..
-	else if (userId.length != 0) $('#guide_id').css("color", "#038900").text("사용가능한 아이디입니다.")
+	if (userId == 'admin') {
+		$('#guide_id').css("color", "#FF0000").text("이미 등록된 아이디 입니다. 다시 입력해주세요!");
+		// 중복확인 버튼이 활성화 상태 
+		$('#id_check').attr("disabled", false);
+		return;
+		//입력값이 admin이라면..
+	} else if (userId.length != 0) {
+		$('#guide_id').css("color", "#038900").text("사용가능한 아이디입니다.");
+		// 중복확인 버튼이 비활성화 상태
+		$('#id_check').attr("disabled", true).css({ "background": "#D1D1D1", "border": "1px solid #D1D1D1" });
+	}
 	//입력값의 길이가 0이 아니라면..(입력은했으나 admin이 아닐때)
-	else alert(`아이디를 입력하세요!`)
+	else alert(`아이디를 입력하세요!`);
 	//입력값이 없을때..
 
 	$("#id").on("propertychange change keyup paste input", function() { //중복체크한후 다른 아이디를 입력했을때
-		$('#guide_id').css("color", "#D1D1D1").text("아이디는 영문과 숫자조합만 가능합니다(4-20자리)")
+		$('#guide_id').css("color", "#D1D1D1").text("아이디는 영문과 숫자조합만 가능합니다(4-20자리)");
+		// 중복확인 버튼이 활성화 상태
+		$('#id_check').attr("disabled", false).css("background", "#EBDCD3");;
 	});
 };
 
 
+//필수값
+function joinButton(){
+	
+		alert(`회원가입이 완료되었습니다 :) `);
+				
+};
+
+//유효성검사
+// id 검증 함수 
+	function validateId(id) {
+		const regExpId = /^[a-z]+[a-z0-9]{4,20}$/g; //변수선언
+
+		return regExpId.test(id); // test => boolean 
+	}
+	// pwd 검증 함수F
+	function validatePwd(pwd) {
+		const regExpPwd = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{10,20}$/;
+		return regExpPwd.test(pwd); // test => boolean
+	}
 
 
-/*
-$('#button').fuction(){
-	if()$(this).
-}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
