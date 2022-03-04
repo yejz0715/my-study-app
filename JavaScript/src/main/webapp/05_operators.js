300876e8cba8f4aa81919bcad044ab3f141bb5e4
yejz0715 <yejz0715@gmail.com>
2022-03-05 02:25:31 +0900

    if

--- JavaScript/src/main/webapp/05_operators.js
+++ JavaScript/src/main/webapp/05_operators.js
@@ -1,4 +1,4 @@
-//operators 연산자
+//operators 연산자 / comparisonOperator 비교연산자
 
 // +, -, *, /, %
 // % 나머지
@@ -21,3 +21,14 @@ num++; //11
 num--; //9
 let result=num++; //10 변수에 10에 들어가고 증가되기때문에 (나중에 증가됨)
 let result=++num; //11 앞에다 써야함 (먼저 증가됨)
+
+//comparisonOperator 비교연산자
+
+// < > <= >= == != 비교연산자를 사용하면 반환값은 항상 boolean형
+
+// == 동등연산자
+const a=1;
+const b="1";
+console.log(a==b); //true 
+//=== 일체연산자
+console.log(a===b); //false '===' 를 사용하면 type도 비교해줌
---
+++ JavaScript/src/main/webapp/06_if.js
@@ -0,0 +1,27 @@
+//조건문
+//if, else, else if
+
+//if
+const age=30;
+if(age > 19){
+	console.log('환영합니다');
+}
+if(age <= 19){
+	console.log('안녕히 가세요');
+}
+//if else
+const age=30;
+if(age > 19){
+	console.log('환영합니다');
+}else{
+	console.log('안녕히 가세요');
+} 
+//else if
+const age=30;
+if(age > 19){
+	console.log('환영합니다');
+}else if(age == 19){
+	console.log('수능 화이팅!');
+}else{
+	console.log('안녕히 가세요');
+} 
