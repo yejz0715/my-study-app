1ab1fb3b720d1c9a047d61c14b58ed6cf8814f7c
unknown <yejz0715@gmail.com>
2022-04-15 00:26:24 +0900

    출력 수정

--- yeji_test/src/main/webapp/javaScript_test/02_ relational operator.js
+++ yeji_test/src/main/webapp/javaScript_test/02_ relational operator.js
@@ -2,7 +2,7 @@
 //문제
 //함수 solution의 내용을 수정해서 a와 b가 모두 True라면 True를, 그렇지 않으면 False를 return하도록 만들어 보세요.
 function solution(a, b) {
-  if (a == true && b == true) return true;
+  if (a && b) return true;
   else return false;
 }
-solution();
+console.log(solution(false, false));
--- yeji_test/src/main/webapp/javaScript_test/03_string.js
+++ yeji_test/src/main/webapp/javaScript_test/03_string.js
@@ -2,7 +2,7 @@
 //문제
 //함수 solution을 수정해서 인자 str의 첫번째 글자를 return하는 함수를 만들어 보세요.
 function solution(str) {
-  var a = str[0];
+  const a = str[0];
   return a;
 }
-console.log(solution());
+console.log(solution("test"));
